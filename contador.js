b555658d5f4c303c619528053bd9eaf51e0e14b1
Leonpsilv <leonardo.pin.s2002@gmail.com>
2022-06-03 18:07:55 -0400

    contador estilizado

--- contador.js
+++ contador.js
@@ -1,11 +1,23 @@
-let numberIncrement = document.querySelector("#number-increment");
+const numberIncrement = document.querySelector("#number-increment");
+
+const colorChange = () => {
+    if (parseInt(numberIncrement.textContent) < 0){
+        numberIncrement.style.color = 'red';
+    }else{
+        numberIncrement.style.color = 'black';
+    }
+    
+}   
 
 const incrementar = () => {
-    let num = parseInt(numberIncrement.textContent);
+    const num = parseInt(numberIncrement.textContent);
     numberIncrement.textContent = num + 1;
+    colorChange();
 }
 
 const decrementar = () => {
     const num = parseInt(numberIncrement.textContent);
     numberIncrement.textContent = num - 1;
+    colorChange();
 }
+
